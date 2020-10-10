18f72825cdf8d347f186562d96fa98d1e1b0bf43
aman909909 <amanashish909@gmail.com>
2020-10-10 16:45:22 +0530

    Add forum

--- general/admin.py
+++ general/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import ForumPost, ForumComment, ForumReply
 
-# Register your models here.
+
+admin.site.register(ForumPost)
+admin.site.register(ForumComment)
+admin.site.register(ForumReply)
--- general/models.py
+++ general/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ForumPost(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    heading = models.CharField(max_length=100)
+    body = models.TextField()
+    date_time = models.DateTimeField(auto_now_add=True)
+
+class ForumComment(models.Model):
+    parent_post = models.ForeignKey(ForumPost, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=200)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    date = models.DateTimeField(auto_now_add=True)
+
+class ForumReply(models.Model):
+    parent_comment = models.ForeignKey(ForumComment, on_delete=models.CASCADE)
+    reply = models.CharField(max_length=200)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    date = models.DateTimeField(auto_now_add=True)
